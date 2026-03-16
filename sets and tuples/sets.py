032acd2d1fb77bff712004522a619407cdf8d35a
defnotutkarsh <justutkarsh27@gmail.com>
2026-03-16 22:40:17 +0530

    okay

--- sets and tuples/sets.py
+++ sets and tuples/sets.py
@@ -1,7 +1,29 @@
-def rev(s):
-    res = {}
+# def rev(s):
+#     res = {}
     
-    for char in s :
-        res[char] = res.get(char,0) +1
-    return res
-print(rev("hello"))
+#     for char in s :
+#         res[char] = res.get(char,0) +1
+#     return res
+# print(rev("hello"))
+
+# def ana(lst):
+#     res = {}
+#     for word in lst:
+#         key = "".join(sorted(word))
+#         if key in res :
+#             res[key].append(word)
+#         else:
+#             res[key] = [word]
+#     return list(res.values())
+# print(ana(["eat", "tea", "tan", "ate", "nat", "bat"]))
+
+def nonrep(s):
+    count = {}
+    for char in s:
+        count[char] = count.get(char,0) + 1
+    for char in s:
+
+        if (count[char] == 1 ):
+            return char
+    return None
+print(nonrep("aabbcde"))
