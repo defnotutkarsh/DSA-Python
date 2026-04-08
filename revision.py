70d71880cd3689f828bc6b311ddde1591c3befda
defnotutkarsh <justutkarsh27@gmail.com>
2026-04-08 10:38:56 +0530

    almost done

--- revision.py
+++ revision.py
@@ -137,4 +137,63 @@
 # print(find_max([]))                      # None
 
 
-def two_sum
+# def two_sum(lst, target):
+#     seen = {}
+#     for i in range(len(lst)):
+#         need = target - lst[i]
+#         if need in seen :
+#             return [seen[need],i]
+#         seen[lst[i]] = i
+#     return[]
+# print(two_sum([2, 7, 11, 15], 9))       # [0, 1]
+# print(two_sum([3, 2, 4], 6))            # [1, 2]
+# print(two_sum([1, 5, 3, 7], 8))         # [1, 3]
+# print(two_sum([10, 20, 30, 40], 50))    # [1, 2]
+
+# def second_largest(lst):
+#     first = lst[0]
+#     second = None
+#     for i in range(len(lst)):
+#         if(lst[i]>first):
+#             second = first
+#             first = lst[i]
+#         elif first > lst[i] and (second is None or lst[i] > second):
+#             second = lst[i]
+#     return second
+# print(second_largest([3, 7, 2, 9, 1]))        # 7
+# print(second_largest([10, 20, 30]))            # 20
+# print(second_largest([5, 5, 5]))               # None
+# print(second_largest([1]))                     # None
+# print(second_largest([4, 1, 8, 8, 3]))         # 4
+
+
+# def freq(s):
+#     s=s.lower().split()
+#     count ={}
+#     for word in s:
+#         count[word] = count.get(word,0)+1
+#     return count
+# print(freq("the cat and the dog"))
+
+# def invert(d):
+    # for key,values in d.items():
+        # key,value = value,key
+        # 
+# def ana(lst):
+#     res = {}
+#     for word in lst:
+#         key = "".join(sorted(word))
+#         if (key in res):
+#             res[key].append(word)
+#         else:
+#             res[key] = [word]
+#     return list(res.values())
+# print(ana(["eat", "tea", "tan", "ate", "nat", "bat"]))
+
+
+# def charmap(s1,s2):
+#     d={}
+#     for i in range (len(s1)):
+#         d[s1[i]] = s2[i]
+#         return 
+
